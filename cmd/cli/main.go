6ceb2c80671d670ecf8f633f95e7e1301257e5a9
pjablonski <piojablonski@gmail.com>
2023-01-31 10:01:40 +0100

    Integrate main with board.Board

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"github.com/piojablonski/quizzgame/board"
+	"log"
 	"os"
 )
 
 func main() {
-	done := make(chan bool)
-	go func() {
-		board.DisplayQuestion(os.Stdin, os.Stdout)
-		done <- true
-	}()
-
-	<-done
+	const filename = "problems.csv"
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0644)
+	if err != nil {
+		log.Fatalf("problem opening file %s, %v", filename, err)
+	}
+	b := board.New(file)
+	err = b.DisplayQuestion(os.Stdin, os.Stdout)
+	if err != nil {
+		log.Fatalf("problem asking questions, %v", err)
+	}
 }
