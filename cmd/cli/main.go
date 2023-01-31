b8b0cdf505e22e2aec992f979e4ad2b23f930fa9
pjablonski <piojablonski@gmail.com>
2023-01-31 10:13:43 +0100

    Add ability to pass a path to csv file with questions through command line argument

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"flag"
 	"github.com/piojablonski/quizzgame/board"
 	"log"
 	"os"
 )
 
+var filename = flag.String("f", "problem.csv", `gives a path to a file with questions, defaults to "problem.csv"`)
+
 func main() {
-	const filename = "problems.csv"
-	file, err := os.OpenFile(filename, os.O_RDONLY, 0644)
+	flag.Parse()
+	file, err := os.OpenFile(*filename, os.O_RDONLY, 0644)
 	if err != nil {
-		log.Fatalf("problem opening file %s, %v", filename, err)
+		log.Fatalf("problem opening file %s, %v", *filename, err)
 	}
 	b := board.New(file)
 	err = b.DisplayQuestion(os.Stdin, os.Stdout)
